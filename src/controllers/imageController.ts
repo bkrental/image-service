c69cbb701be8d3319f381b9758f5c9697618050f
vide_y_sora <nguyenphuoc2508.lhp.bk@gmail.com>
2024-04-05 16:28:11 +0700

    [Style] remove comments

--- src/controllers/imageController.ts
+++ src/controllers/imageController.ts
@@ -3,15 +3,6 @@ import ImageService from "../services/ImageService";
 
 const ImageController = {
   uploadImage: async (req: Request, res: Response) => {
-    // const { imagePath } = req.body;
-
-    // // const buffer = Buffer.from(imagePath, "base64");
-
-    // const imageService = new ImageService();
-    // console.log(imagePath);
-    // const uploadImage = await imageService.uploadImage(imagePath);
-    // console.log(uploadImage);
-    // res.send(uploadImage);
     if (!req.file) {
       return res.status(400).json({ error: "No file uploaded" });
     }
