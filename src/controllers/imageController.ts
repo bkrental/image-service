9ce3d79a06f158454e823ccb9a6f47dd0628019d
vide_y_sora <nguyenphuoc2508.lhp.bk@gmail.com>
2024-04-08 15:32:33 +0700

    [Feat] add multiple image upload api

--- src/controllers/imageController.ts
+++ src/controllers/imageController.ts
@@ -14,6 +14,25 @@ const ImageController = {
       url: uploadImage.url,
     });
   },
+
+  uploadMultipleImages: async (req: Request, res: Response) => {
+    if (!req.files) {
+      return res.status(400).json({ error: "No file uploaded" });
+    }
+
+    const imageService = new ImageService();
+    const uploadImagesPromises = (req.files as Express.Multer.File[]).map(
+      (file) => {
+        return imageService.uploadImage(file);
+      }
+    );
+
+    const uploadImages = await Promise.all(uploadImagesPromises);
+
+    return res.json({
+      urls: uploadImages.map((uploadImage) => uploadImage.url),
+    });
+  },
 };
 
 export default ImageController;
